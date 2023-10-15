f390df88b21414e4d4a28f5ccf3ae64c7ca7dd09
achilezweb@gmail.com <achilezweb@gmail.com>
2023-10-15 22:04:32 +0800

    added logout link

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,10 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <a class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
     </x-slot>
 
     <div class="py-12">
